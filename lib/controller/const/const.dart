09e6a4c1f6903a9da6bca9cc33d77e1472891c4e
Manojkumar <muhammed.safvan@credr.com>
2024-01-08 17:32:31 +0530

    app bar ui

---
+++ lib/controller/const/const.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+TextStyle appBarTitleStyle = const TextStyle(
+    fontSize: 15, fontWeight: FontWeight.w500, color: Colors.black);
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webapp/view/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'web',
       theme: ThemeData(
+        focusColor: Colors.black,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const HomeView(),
     );
   }
 }
